24e82302b2fe533e5c009f19cc549200dd326830
Ingo Schwarz <schwarz-ingo@web.de>
2016-11-16 06:35:16 +0100

    Rename data to arg in all classes

--- src/main/java/de/codecrafters/utils/listenersupport/ListenerSupport1.java
+++ src/main/java/de/codecrafters/utils/listenersupport/ListenerSupport1.java
@@ -7,29 +7,29 @@ import java.util.concurrent.Executor;
  * Implementation of ListenerSupport for callback method with one argument.
  *
  * @param <L> The type of the listener that shall be notified.
- * @param <D> The type of the argument that shall be passed to the callback method.
+ * @param <A> The type of the argument that shall be passed to the callback method.
  * @author ISchwarz
  */
-public final class ListenerSupport1<L, D> extends ListenerSupportBase<L> {
+public final class ListenerSupport1<L, A> extends ListenerSupportBase<L> {
 
     /**
      * Interface for a listener notifiers with a callback methods with one argument.
      *
      * @param <L> The type of the listener that shall be notified.
-     * @param <D> The type of the argument that shall be passed to the callback method.
+     * @param <A> The type of the argument that shall be passed to the callback method.
      */
-    public interface ListenerNotifier<L, D> {
+    public interface ListenerNotifier<L, A> {
 
         /**
-         * Passes the given data to the given listener.
+         * Passes the given argument to the given listener.
          *
          * @param listener The listener that shall be notified.
-         * @param data     The data that shall be passed to the listener.
+         * @param arg      The argument that shall be passed to the listener.
          */
-        void notifyListener(L listener, D data);
+        void notifyListener(L listener, A arg);
     }
 
-    private final ListenerNotifier<L, D> notifier;
+    private final ListenerNotifier<L, A> notifier;
 
     /**
      * Creates a new ListenerSupport that uses the given {@link ListenerNotifier} and the given {@link Executor}.
@@ -37,7 +37,7 @@ public final class ListenerSupport1<L, D> extends ListenerSupportBase<L> {
      * @param notifier       The {@link ListenerNotifier} that shall be used to notify the listeners.
      * @param notifyExecutor The {@link Executor} that shall be used to trigger the notification.
      */
-    ListenerSupport1(final ListenerNotifier<L, D> notifier, final Executor notifyExecutor) {
+    ListenerSupport1(final ListenerNotifier<L, A> notifier, final Executor notifyExecutor) {
         super(notifyExecutor);
         this.notifier = notifier;
     }
@@ -45,13 +45,13 @@ public final class ListenerSupport1<L, D> extends ListenerSupportBase<L> {
     /**
      * Notifies the registered listeners with the given argument.
      *
-     * @param data The data that shall be passed to the listeners.
+     * @param arg The argument that shall be passed to the listeners.
      */
-    public void notifyListeners(final D data) {
-        getListeners().forEach(listener -> notifyListener(listener, data));
+    public void notifyListeners(final A arg) {
+        getListeners().forEach(listener -> notifyListener(listener, arg));
     }
 
-    private void notifyListener(L listener, D data) {
-        notifyListenerInExecutor(listener, () -> notifier.notifyListener(listener, data));
+    private void notifyListener(L listener, A arg) {
+        notifyListenerInExecutor(listener, () -> notifier.notifyListener(listener, arg));
     }
 }
--- src/main/java/de/codecrafters/utils/listenersupport/ListenerSupport3.java
+++ src/main/java/de/codecrafters/utils/listenersupport/ListenerSupport3.java
@@ -5,24 +5,24 @@ import java.util.concurrent.Executor;
 /**
  * Created by Ingo on 12.11.2016.
  */
-public final class ListenerSupport3<L, D1, D2, D3> extends ListenerSupportBase<L> {
+public final class ListenerSupport3<L, A1, A2, A3> extends ListenerSupportBase<L> {
 
-    public interface ListenerNotifier<L, D1, D2, D3> {
-        void notifyListener(L listener, D1 data1, D2 data2, D3 data3);
+    public interface ListenerNotifier<L, A1, A2, A3> {
+        void notifyListener(L listener, A1 arg1, A2 arg2, A3 arg3);
     }
 
-    private final ListenerNotifier<L, D1, D2, D3> notifier;
+    private final ListenerNotifier<L, A1, A2, A3> notifier;
 
-    ListenerSupport3(final ListenerNotifier<L, D1, D2, D3> notifier, final Executor notifyExecutor) {
+    ListenerSupport3(final ListenerNotifier<L, A1, A2, A3> notifier, final Executor notifyExecutor) {
         super(notifyExecutor);
         this.notifier = notifier;
     }
 
-    public void notifyListeners(final D1 data1, final D2 data2, final D3 data3) {
-        getListeners().forEach(listener -> notifyListener(listener, data1, data2, data3));
+    public void notifyListeners(final A1 arg1, final A2 arg2, final A3 arg3) {
+        getListeners().forEach(listener -> notifyListener(listener, arg1, arg2, arg3));
     }
 
-    private void notifyListener(L listener, D1 data1, D2 data2, D3 data3) {
-        notifyListenerInExecutor(listener, () -> notifier.notifyListener(listener, data1, data2, data3));
+    private void notifyListener(L listener, A1 arg1, A2 arg2, A3 arg3) {
+        notifyListenerInExecutor(listener, () -> notifier.notifyListener(listener, arg1, arg2, arg3));
     }
 }
