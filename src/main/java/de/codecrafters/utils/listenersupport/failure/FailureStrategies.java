5bcdb25772e1ad3b2b040f1b094381dee38fcd01
Ingo Schwarz <schwarz-ingo@web.de>
2016-11-16 19:45:35 +0100

    Finish FailureStrategy factory

--- src/main/java/de/codecrafters/utils/listenersupport/failure/FailureStrategies.java
+++ src/main/java/de/codecrafters/utils/listenersupport/failure/FailureStrategies.java
@@ -1,9 +1,9 @@
 package de.codecrafters.utils.listenersupport.failure;
 
-import org.apache.logging.log4j.Level;
-
 /**
- * Created by Ingo on 16.11.2016.
+ * A collection of predefined {@link FailureStrategy}s.
+ *
+ * @author ISchwarz
  */
 public final class FailureStrategies {
 
@@ -11,12 +11,43 @@ public final class FailureStrategies {
         //no instance
     }
 
+    /**
+     * Creates a new {@link FailureStrategy} that uses the Log4j {@link org.apache.logging.log4j.Logger} to log the
+     * thrown errors in {@code ERROR} level.
+     *
+     * @return The created {@link FailureStrategy}.
+     */
     public static FailureStrategy<Object> createLog4jLogger() {
-        return new Log4jFailureStrategy(Level.ERROR);
+        return createLog4jLogger(org.apache.logging.log4j.Level.ERROR);
     }
 
-    public static FailureStrategy<Object> createLog4jLogger(final Level logLevel) {
+    /**
+     * Creates a new {@link FailureStrategy} that uses the Log4j {@link org.apache.logging.log4j.Logger} to log the
+     * thrown errors in the given log level.
+     *
+     * @return The created {@link FailureStrategy}.
+     */
+    public static FailureStrategy<Object> createLog4jLogger(final org.apache.logging.log4j.Level logLevel) {
         return new Log4jFailureStrategy(logLevel);
     }
 
+    /**
+     * Creates a new {@link FailureStrategy} that uses the Java {@link java.util.logging.Logger} to log the
+     * thrown errors in the {@code FINEST} log level.
+     *
+     * @return The created {@link FailureStrategy}.
+     */
+    public static FailureStrategy<Object> createJavaLogger() {
+        return createJavaLogger(java.util.logging.Level.FINEST);
+    }
+
+    /**
+     * Creates a new {@link FailureStrategy} that uses the Java {@link java.util.logging.Logger} to log the
+     * thrown errors in the given log level.
+     *
+     * @return The created {@link FailureStrategy}.
+     */
+    public static FailureStrategy<Object> createJavaLogger(final java.util.logging.Level logLevel) {
+        return new JavaLoggingFailureStrategy(logLevel);
+    }
 }
