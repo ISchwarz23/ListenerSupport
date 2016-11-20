b699ab4b2dcce8d6263de70a8084724122666242
Ingo Schwarz <schwarz-ingo@web.de>
2016-11-20 12:10:03 +0100

    Create FailureStrategyPlugin for SystemErrorWriter

---
+++ failurestrategy-systemerrorwriter/src/main/java/de/codecrafters/listenersupport/failure/SystemErrorFailureStrategyPlugin.java
@@ -0,0 +1,19 @@
+package de.codecrafters.listenersupport.failure;
+
+/**
+ * A {@link FailureStrategyPlugin} implementation that provides the {@link SystemErrorFailureStrategy}.
+ *
+ * @author ISchwarz
+ */
+public final class SystemErrorFailureStrategyPlugin implements FailureStrategyPlugin {
+
+    @Override
+    public String getName() {
+        return SystemErrorFailureStrategy.class.getSimpleName();
+    }
+
+    @Override
+    public FailureStrategy<?> createNewInstance() {
+        return new SystemErrorFailureStrategy();
+    }
+}
